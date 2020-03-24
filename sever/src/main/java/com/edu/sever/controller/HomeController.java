696fecdf9e503c258646dddd933e59c5b017748f
phamphuc1301 <phamphuc224@gmail.com>
2020-03-24 22:52:35 +0700

    init project for both

---
+++ sever/src/main/java/com/edu/sever/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.edu.sever.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping(value = "/home")
+    public String home() {
+        return "forward:/index.html";
+    }
+
+}
